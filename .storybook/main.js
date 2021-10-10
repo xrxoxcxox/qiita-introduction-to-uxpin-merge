179c933916c7f175a2dbd766a8a1a1b107765b9f
xrxoxcxox <watanuki@ju-zen.com>
2021-10-10 18:09:52 +0900

    Apply Emotion to Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  babel: (config) => {
+    config.presets.push(require.resolve('@emotion/babel-preset-css-prop'))
+    return config
+  }
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
-import './button.css'
+import { css } from '@emotion/react'
 
-interface ButtonProps {
+export interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
    */
@@ -20,6 +20,7 @@ interface ButtonProps {
   /**
    * Optional click handler
    */
+  className?: string
   onClick?: () => void
 }
 
@@ -31,17 +32,46 @@ export const Button = ({
   size = 'medium',
   backgroundColor,
   label,
+  className,
   ...props
 }: ButtonProps): JSX.Element => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary'
+  const mode = primary ? styles.primary : styles.secondary
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
+    <button type="button" css={[styles.button, styles[size], mode, className]} style={{ backgroundColor }} {...props}>
       {label}
     </button>
   )
+}
+
+const styles = {
+  button: css`
+    font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-weight: 700;
+    border: 0;
+    border-radius: 3em;
+    cursor: pointer;
+    display: inline-block;
+    line-height: 1;
+  `,
+  primary: css`
+    color: white;
+    background-color: #1ea7fd;
+  `,
+  secondary: css`
+    color: #333;
+    background-color: transparent;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+  `,
+  small: css`
+    font-size: 12px;
+    padding: 10px 16px;
+  `,
+  medium: css`
+    font-size: 14px;
+    padding: 11px 20px;
+  `,
+  large: css`
+    font-size: 16px;
+    padding: 12px 24px;
+  `
 }
