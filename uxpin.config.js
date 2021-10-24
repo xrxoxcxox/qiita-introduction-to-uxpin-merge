38ea69e0ca8dea9eeeaa24a61be76180da97102a
xrxoxcxox <watanuki@ju-zen.com>
2021-10-24 15:55:39 +0900

    Add Card component

--- uxpin.config.js
+++ uxpin.config.js
@@ -5,6 +5,7 @@ module.exports = {
         name: 'General',
         include: [
           'src/components/Button/Button.tsx',
+          'src/components/Card/Card.tsx',
           'src/components/Textfield/Textfield.tsx',
           'src/components/Typography/Typography.tsx'
         ]
